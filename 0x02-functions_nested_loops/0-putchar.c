2d108c478d78c410db8d6c80efa694dca508dade
Igli-Boci <igliboci10@gmail.com>
2022-02-08 03:12:00 -0800

    First test of header file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#inlcude <"main.h">
+/**
+ * main -  program that prints all possible combinations of single-digit
+ * Numbers must be separated by ,followed by a space
+ * Numbers should be printed in ascending order
+ * You can only use the putchar function
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar(95);
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+	return (0);
+}
