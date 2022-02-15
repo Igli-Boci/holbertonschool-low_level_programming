d5316b671cac5914a22a07ff6eaa5a87ef3c9859
Igli-Boci <igliboci10@gmail.com>
2022-02-15 05:47:09 -0800

    Function that converts a string into an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -21,16 +21,8 @@ int _atoi(char *s)
 	}
 	while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
 	{
-		if (n >= 0)
-		{
-			n = n * 10 - (s[i] - '0');
-			i++;
-		}
-		else
-		{
-			n = n * 10 - (s[i] - '0');
-			i++;
-		}
+		n = n * 10 - (s[i] - '0');
+		i++;
 	}
 	signo *= -1;
 	return (n * signo);
