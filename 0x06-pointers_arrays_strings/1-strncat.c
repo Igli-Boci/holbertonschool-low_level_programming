5c7222e5bf146aed78be4944a69a6ab1bb1833bf
Igli-Boci <igliboci10@gmail.com>
2022-02-17 06:02:01 -0800

    Function that concatenates 2 strings with n bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncat - Function that concatenates 2 string with n bytes
+ * @dest: Pointer
+ * @src: Pointer
+ * @n: Bytes
+ * Return: Dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int k;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	k = 0;
+	while (src[k] != '\0' && < n)
+	{
+		dest[i] = src[k];
+		k++;
+		i++;
+	}
+	return (dest);
+}
