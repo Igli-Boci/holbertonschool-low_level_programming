d6021f6e3bbbb204a682911eed74a911daac8bae
Igli-Boci <igliboci10@gmail.com>
2022-02-21 13:09:29 -0800

    Strbrk function that matches bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strbrk - Function that searches for any set of bytes in a string
+ * @s: Pointer
+ * @accept: Bytes
+ * Return: s or '\0'
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int l = 0;
+	int k = 0;
+	int m;
+
+	while (s[l] != '\0')
+	{
+		m = 0;
+		while (accept[k] != '\0')
+		{
+			if (s[l] == accept[k])
+			{
+				m = 1;
+			}
+			k++;
+		}
+		k = 0;
+		if (m == 1)
+		{
+			return (s);
+		}
+		l++;
+	}
+	return ('\0');
+}
