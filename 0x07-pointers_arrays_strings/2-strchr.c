072cff5e638dd7760d1d9b56c876a17c6c92173d
Igli-Boci <igliboci10@gmail.com>
2022-02-21 06:07:58 -0800

    Strchr function

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - Function that locates a char in a str
+ * @s: String
+ * @c: Char
+ * Return: s
+ */
+
+char *_strchr(char *s, char c)
+{
+	int l = 0;
+
+	while (s[l] != '\0')
+	{
+		if (s[l] == c)
+		{
+			return (s);
+		}
+		else
+		{
+			return ('\0');
+		}
+		l++;
+	}
+}
