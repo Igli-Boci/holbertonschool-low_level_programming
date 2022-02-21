a5b4504501b1fdd297691d1260870179b6f5f2e1
Igli-Boci <igliboci10@gmail.com>
2022-02-21 14:22:50 -0800

    Strpbrk function that matches bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -20,12 +20,10 @@ char *_strpbrk(char *s, char *accept)
 		{
 			if (s[l] == accept[k])
 			{
-				m = 0;
+				return (s + l);
 			}
 			k++;
 		}
-		if (m == 1)
-			return (s);
 		l++;
 	}
 	return ('\0');
