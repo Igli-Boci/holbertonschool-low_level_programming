9ea7d98769745570198b47a6498df3d48b911741
Igli-Boci <igliboci10@gmail.com>
2022-03-09 03:25:19 -0800

    Pointer to a function that prints a name, fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -9,8 +9,8 @@
  * Return: Nothing
  */
 
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
+	if (name =! 0 || f =! 0)
 		f(name);
 }
