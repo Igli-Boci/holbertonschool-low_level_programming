12fdf622c5f7bc3e6eded004de8e0c720ff3dd1e
Igli-Boci <igliboci10@gmail.com>
2022-03-09 07:09:33 -0800

    Program that performs simple operations, fix

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,13 +18,13 @@ int (*get_op_func(char *s))(int, int)
 		{"/", op_div},
 		{"%", op_mod},
 		{NULL, NULL}
-	}
-	int i;
+	};
 
-	i = 0;
+	int i = 0;
+
 	while (ops[i].op != NULL)
 	{
-		if (*(ops[i]).op == *s)
+		if (*ops[i].op == *s)
 		{
 			return (ops[i].f);
 		}
