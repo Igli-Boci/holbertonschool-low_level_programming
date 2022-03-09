9287d5ecffd9eebf8247ea87f5019d47b3a04b45
Igli-Boci <igliboci10@gmail.com>
2022-03-09 03:01:49 -0800

    Pointer to a function that prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - Function that points to the function in main
+ * @name: Name
+ * @f: Function
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *));
+{
+	if (name != 0 && f != 0)
+		f(name);
+}
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterrator - Parameter of each element of an array.
+ * @array: Array
+ * @size: Size
+ * @action: Pointer to a function
+ * Return: Nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int));
+{
+	size_t = l;
+
+	l = 0;
+	while (l < size)
+	{
+		action(array[l]);
+		i++;
+	}
+
+}
